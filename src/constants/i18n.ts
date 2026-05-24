7d00605393ddddab00e7a57ae89906b8831391b6
Vladyslava <vladaboychuk@knu.ua>
2026-05-24 21:33:35 +0000

    simplify country translation to be fully dynamic using

--- src/constants/i18n.ts
+++ src/constants/i18n.ts
@@ -22,67 +22,8 @@ export function t(lang?: string) {
   return uk;
 }
 
-const UKRAINIAN_COUNTRIES: Record<string, string> = {
-  UA: 'Україна',
-  US: 'США',
-  GB: 'Велика Британія',
-  PL: 'Польща',
-  DE: 'Німеччина',
-  FR: 'Франція',
-  IT: 'Італія',
-  ES: 'Іспанія',
-  CA: 'Канада',
-  TR: 'Туреччина',
-  EG: 'Єгипет',
-  RO: 'Румунія',
-  HU: 'Угорщина',
-  SK: 'Словаччина',
-  CZ: 'Чехія',
-  AT: 'Австрія',
-  CH: 'Швейцарія',
-  BE: 'Бельгія',
-  NL: 'Нідерланди',
-  SE: 'Швеція',
-  NO: 'Норвегія',
-  FI: 'Фінляндія',
-  DK: 'Данія',
-  GR: 'Греція',
-  PT: 'Португалія',
-  CN: 'Китай',
-  JP: 'Японія',
-  IN: 'Індія',
-  BR: 'Бразилія',
-  MX: 'Мексика',
-  AU: 'Австралія',
-  KR: 'Південна Корея',
-  RU: 'РФ',
-  BY: 'Білорусь',
-  KZ: 'Казахстан',
-  GE: 'Грузія',
-  AM: 'Вірменія',
-  AZ: 'Азербайджан',
-  MD: 'Молдова',
-  IL: 'Ізраїль',
-  IE: 'Ірландія',
-  NZ: 'Нова Зеландія',
-  ZA: 'ПАР',
-  SG: 'Сінгапур',
-  TH: 'Таїланд',
-  VN: "В'єтнам",
-  AE: 'ОАЕ',
-  SA: 'Саудівська Аравія',
-  HR: 'Хорватія',
-  BG: 'Болгарія',
-  LT: 'Литва',
-  LV: 'Латвія',
-  EE: 'Естонія',
-};
-
 export function translateCountry(countryCode: string, lang: string): string {
   const code = countryCode.toUpperCase();
-  if (lang === 'uk' && UKRAINIAN_COUNTRIES[code]) {
-    return UKRAINIAN_COUNTRIES[code];
-  }
   try {
     const names = new Intl.DisplayNames([lang], { type: 'region' });
     return names.of(code) || countryCode;
